1c3d9f0882d7caf6e618f3ab084e91b3d6f3ea5a
XiangHY <3560037741@qq.com>
2026-08-16 09:17:04 +0800

    Fix include statement and update output format

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,4 +1,5 @@
 #include "../include/HelloWorld.hpp"
+#include<iostream>
 using namespace std;
 void HelloWorld::myPrint(string name, int age){
     cout << "Hello World, " <<"my name is "<<"{"<< name << "}"<<"my age is "<<"{" << age <<"}"<< "." <<endl;
